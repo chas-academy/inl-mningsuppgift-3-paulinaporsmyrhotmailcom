b5425038f6025f0bb3f9b69dc7c0bcb0b61f573b
paulina.porsmyr@hotmail.com <paulina.porsmyr@hotmail.com>
2025-04-09 15:57:51 +0200

    First commit

--- main.c
+++ main.c
@@ -2,19 +2,20 @@
 #include <math.h>
 
 
-// This is where you add the function calculate_hypotenuse
+float calculate_hypotenuse(float a, float b){
+    float c = sqrt(pow(a, 2)+pow(b, 2));
+    return c;
+}
 
-int main() {
+int main(){
     float a = 3.0;
     float b = 4.0;
 
-    printf("%.2f\n", calculate_hypotenuse(a, b));  // Expected output: 5.00
-
-    //Changing the values
+    printf("%.2f\n", calculate_hypotenuse(a, b)); 
     a = 5.0;
     b = 12.0;
 
-    printf("%.2f\n", calculate_hypotenuse(a, b));  // Expected output: 13.00
+    printf("%.2f\n", calculate_hypotenuse(a, b)); 
 
     return 0;
 }
